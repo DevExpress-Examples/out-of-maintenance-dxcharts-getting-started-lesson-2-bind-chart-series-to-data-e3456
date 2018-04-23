89a6027dc99cc28a2bbcce558a0433efd91f3591
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/SilverlightApplication2/MainPage.xaml.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Charts
+
+
+Namespace SilverlightApplication2
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+	End Class
+End Namespace
