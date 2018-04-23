c26427de24ab6932fc2b4ae3a459c552ca90c06d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SilverlightApplication2/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Charts;
+
+
+namespace SilverlightApplication2 {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+        }
+       
+    }
+}
